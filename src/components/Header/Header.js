5ede2355328aef9a0b203a7d7f06b922cd40cecd
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-10-18 11:32:01 +0600

    google sign in  done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import logo from '../../images/logo.png';
-import useAuth from '../../../hooks/useAuth';
 import { useHistory } from 'react-router';
+import useAuth from '../../hooks/useAuth';
 
 const Header = () => {
     const { user, logOut } = useAuth();
@@ -14,10 +14,6 @@ const Header = () => {
     const handleSignUp = () => {
         history.push("/register")
     }
-
-    const handleShipping = () => {
-        history.push("/shipping")
-    }
     const handleNavbarBand = () => {
         history.push("/home")
     }
@@ -26,28 +22,27 @@ const Header = () => {
         <>
             <Navbar variant="light">
                 <Container>
-                    <Navbar.Brand className="d-flex align-items-center" href="">
+                    <Navbar.Brand href="" onClick={handleNavbarBand}>
                         <img
                             src={logo}
-                            width="100"
-                            height="60"
+                            width="130"
+                            height="40"
                             className="d-inline-block align-top"
                             alt="React Bootstrap logo"
                         />
-                        <h3>Doctors Lab Medical Center</h3>
                     </Navbar.Brand>
                     <Nav className="ms-auto">
-                        <Navbar.Text>
-                            Signed in as: <a href="#login"></a>
-                            <Button variant="danger" className="mx-3 rounded-pill px-4">Log Out</Button>
+                        {user?.email ? <Navbar.Text>
+                            Signed in as: <a href="#login">{user.displayName}</a>
+                            <Button onClick={logOut} variant="danger" className="mx-3 rounded-pill px-4">Log Out</Button>
                         </Navbar.Text>
+                            :
+                            <div className="d-flex">
 
-                        <div className="d-flex">
-
-                            <Button variant="danger" className="mx-3 rounded-pill px-4">Login</Button>
-                            <Button variant="danger" className="rounded-pill px-4">Sign up</Button>
-                        </div>
-
+                                <Button onClick={handleLogIn} variant="danger" className="mx-3 rounded-pill px-4">Login</Button>
+                                <Button onClick={handleSignUp} variant="danger" className="rounded-pill px-4">Sign up</Button>
+                            </div>
+                        }
                     </Nav>
                 </Container>
             </Navbar>
